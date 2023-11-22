64133f54b515e873ecd76ead3b418c3df84d9e4a
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-11-22 22:44:46 +0530

    video background stucture && addTrailerVideo action created in movieSlice

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/movieSlice";
 
-const VideoBackground = () => {
+const VideoBackground = ({ movieId }) => {
+  const dispatch = useDispatch();
+
+  // Fetch trailer video
+  const getMoviesVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/670292/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0]; // If there is no trailer available then It will show the first tailer/clip whatever it find
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMoviesVideos();
+  }, []);
+
   return (
-    <div>VideoBackground</div>
-  )
-}
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/"}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
 
-export default VideoBackground
+export default VideoBackground;
--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -4,14 +4,19 @@ const movieSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    trailerVideo: null,
   },
   reducers: {
     addNowMoviesPlaying: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
   },
 });
 
-export const { addNowMoviesPlaying } = movieSlice.actions;
+export const { addNowMoviesPlaying, addTrailerVideo } = movieSlice.actions;
 
 export default movieSlice.reducer;
