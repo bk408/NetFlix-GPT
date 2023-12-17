e0466f827213c01623e7fb4e748da2ca32c68e5f
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-12-17 17:00:21 +0530

    Netflix Favicon added

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -12,7 +12,7 @@ const SecondaryContainer = () => {
           <MovieList title={"Popular"} movies={movies.popularMovies} />
           <MovieList title={"Top Rated"} movies={movies.topratedMovies} />
           <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
-          <MovieList title={"Latest Movies"} movies={movies.latestMovies} />
+          {/*<MovieList title={"Latest Movies"} movies={movies.latestMovies} />*/}
         </div>
       </div>
     )
