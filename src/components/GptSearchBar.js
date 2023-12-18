9fdd404aa9f5cbb16252f18712e27c276de5db11
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-12-19 00:27:23 +0530

    new seacrh bar and search button created

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const GptSearchBar = () => {
   return (
     <div className="pt-[9%] flex justify-center">
-      <form className=" bg-black w-1/2  grid grid-cols-12">
-        <input
-          type="text"
-          placeholder="What would you like to watch today?"
-          className="p-4 m-1 col-span-10"
-        />
-        <button className="py-2 px-2 m-2 bg-red-700  text-white rounded-md col-span-2">
-          Search
-        </button>
+      <form className="bg-white bg-opacity-75 w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 rounded-full overflow-hidden">
+        <div className="flex items-center border-b border-gray-600 py-2">
+          <input
+            type="text"
+            placeholder="Search for movies, shows, and more..."
+            className="p-4 w-full bg-transparent text-black outline-none placeholder-black"
+          />
+          <button className="py-2 px-8 mr-7 bg-red-700 text-white rounded-full hover:bg-red-600 transition duration-300">
+            Search
+          </button>
+        </div>
       </form>
     </div>
   );
-}
+};
 
-export default GptSearchBar
+export default GptSearchBar;
